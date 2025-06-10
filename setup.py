130b6bad608b5467d3cb5a1f9b8712772511ca36
Bit-Halloween <edu-barreda17@hotmail.com>
2025-06-10 00:28:42 -0300

    :wrench: Configurando o setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
-# pip install cx_freeze
-import cx_Freeze
-executaveis = [ 
-               cx_Freeze.Executable(script="feito.py", icon="assets/icone.ico") ]
-cx_Freeze.setup(
-    name = "pygame_app",
-    version = "0.1",
-    options={
-        "build_exe":{
-            "packages":["pygame"],
-            "include_files":["assets"]
-        }
-    }, executables = executaveis
-)
+from cx_Freeze import setup, Executable
+import os
 
+executaveis = [
+    Executable(
+        script="main_simpsons_com_verduras_py.py",
+        icon="recursos/assets/icone_simpsons.ico"
+    )
+]
+
+setup(
+    name="Paygame Simpsons",
+    version="1.0",
+    description="Jogo criado com Pygame",
+    options={
+        "build_exe": {
+            "packages": ["pygame", "tkinter", "json", "random"],
+            "include_files": [
+                ("recursos/assets", "recursos/assets"),
+                ("funcoes.py", "recursos/funcoes.py"),
+                "base.atitus"
+            ]
+        }
+    },
+    executables=executaveis
+)
+# Este script é usado para empacotar o jogo usando cx_Freeze.
+# Certifique-se de que cx_Freeze está instalado no seu ambiente Python.
+# Você pode instalar cx_Freeze usando o comando: pip install cx_Freeze
+# Para construir o executável, execute o seguinte comando no terminal:
 # python setup.py build
-# python setup.py bdist_msi
